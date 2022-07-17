50436731474138b22b741f73cb9767861f6a9ac5
biankdev <biancapelcastrevarela@gmail.com>
2022-07-17 12:01:35 -0500

    operaciones basicas

--- js-basico/RetosCasa/EjeSumarBotom/sumaboton.js
+++ js-basico/RetosCasa/EjeSumarBotom/sumaboton.js
@@ -1,42 +1,52 @@
 var input = document.getElementById("num1"); // GUARDO EL OBJETO INPUT DE HTML EN UNA VARIABLE 
 var input2 = document.getElementById("num2");
-
-console.log(input, input2);
+var mostrarRes = document.getElementById("res-el");
+ 
+console.log(input, input2);//ayuda a verlos en la consola 
 
 
 function agregar (){
-    var numero1 = input.value;  //accedo a su valor del imput
+    var numero1 = parseInt(input.value)  //accedo a su valor del imput
     console.log(numero1)
     return numero1
 }
 
-
+        //reserva un valor en la memoria, para guardar el valor 
 function agregar2 (){
-    var numero2 = input2.value;
+    var numero2 = parseInt(input2.value) //valida el valor si es nuero o string
     console.log(numero2)
-    return numero2
+    return numero2 // retorna el numero a la funcion agregar2
 }
 
 
 var sumar = function (){
-
-    return  alert( agregar() + agregar2())  
+    var res = agregar() + agregar2()  
+    var text = res.toString();
+    mostrarRes.innerText = text
 }
 
-var restar = function ( c , d){
-    var c = parseInt(prompt("inserta el num"));
-    var d = parseInt(prompt("inserta el num"));
-    return alert( c - d )
+var restar = function (){
+   
+    var res =  agregar() - agregar2 ()
+    var text = res.toString();
+    mostrarRes.innerText = text
 }
 
-var multiplicar = function(e , f){
-    var e = parseInt(prompt("inserta el num"));
-    var f = parseInt(prompt("inserta el num"));
-    return alert(e * f)
+var multiplicar = function(){
+
+    var res = agregar() * agregar2()
+    var text = res.toString();
+    mostrarRes.innerText = text
 }
 
-var dividir = function(g , h){
-    var g =parseInt(prompt("inserta el num"));
-    var h =parseInt(prompt("inserta el num"));
-    return alert (g / h)
+var dividir = function(){
+ 
+    var res = agregar() / agregar2()
+
+    var text = res.toString(); // toString(); convierte numero a texto
+   
+    mostrarRes.innerText = text // innerText agrega el texto aun elemeto HTML 
+
+
 }
+
